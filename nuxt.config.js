144a81975a3da989703128e43982a01367ad74b7
Mark Ryan Guamos <ryan.guamos@gmail.com>
2021-08-26 19:04:14 +0800

    fixed webpack

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,13 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  extend(config, { isDev, isClient }) {
+    config.externals = [
+      {
+        electron: 'electron',
+      },
+    ]
+  },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - spctracker',
