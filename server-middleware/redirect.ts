ea073450b6033cdeba641caf119b6c1df30aecd2
Mark Ryan Guamos <ryan.guamos@gmail.com>
2021-08-29 17:11:38 +0800

    Revert "Revert "added redirect""
    This reverts commit 300d5d04d626b06cf91abbeddc9127c4ebb6e52d.

---
+++ server-middleware/redirect.ts
@@ -0,0 +1,16 @@
+module.exports = function (req: any, res: any, next: any) {
+  const host = req.headers.host
+
+  const url = req.url
+
+  const env = process.env.NODE_ENV
+
+  const canonicalDomain = 'www.spctracker.info'
+
+  if (env === 'production' && host !== canonicalDomain) {
+    res.writeHead(301, { Location: 'https://' + canonicalDomain + url })
+    return res.end()
+  }
+
+  return next()
+}
