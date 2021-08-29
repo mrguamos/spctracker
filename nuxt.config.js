0b83f8801823116b41d18d304d5323bf0953b472
Mark Ryan Guamos <ryan.guamos@gmail.com>
2021-08-29 21:23:59 +0800

    modified title

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,8 +9,7 @@ export default {
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - spctracker',
-    title: 'spctracker',
+    title: 'SpacePort Tracker',
     htmlAttrs: {
       lang: 'en',
     },
