5f24b7668cbc7742c7bf32b32f2901f04ab1e649
Mark Ryan Guamos <ryan.guamos@gmail.com>
2021-08-29 21:02:34 +0800

    added redirect to new domain

--- server-middleware/redirect.ts
+++ server-middleware/redirect.ts
@@ -5,7 +5,7 @@ module.exports = function (req: any, res: any, next: any) {
 
   const env = process.env.NODE_ENV
 
-  const canonicalDomain = 'www.spctracker.info'
+  const canonicalDomain = 'www.spctracker.com'
 
   if (env === 'production' && host !== canonicalDomain) {
     res.writeHead(301, { Location: 'https://' + canonicalDomain + url })
