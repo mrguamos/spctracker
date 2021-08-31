3b5307ea73e9c1ee69c2c5eaeb4c02e3de300fa2
Mark Ryan Guamos <ryan.guamos@gmail.com>
2021-08-31 21:49:31 +0800

    fixed cors issue on mobile

--- plugins/spc.ts
+++ plugins/spc.ts
@@ -1,13 +1,9 @@
-export default ({ $axios }: any, inject: any) => {
+import axios from 'axios'
+
+export default ({}: any, inject: any) => {
   inject('getSPC', async () => {
-    return $axios.get(
-      'https://api.pancakeswap.info/api/v2/tokens/0x21ea8618b9168eb8936c3e02f0809bbe901282ac',
-      {
-        headers: {
-          'User-Agent':
-            'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
-        },
-      }
+    return axios.get(
+      'https://api.pancakeswap.info/api/v2/tokens/0x21ea8618b9168eb8936c3e02f0809bbe901282ac'
     )
   })
 }
